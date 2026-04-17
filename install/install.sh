32b8bc00418dae9b79cee228a2d4add9e3eb9795
Christopher Schnick <crschnick@xpipe.io>
2026-04-17 15:47:57 +0200

    Fixes

--- install/install.sh
+++ install/install.sh
@@ -17,6 +17,8 @@ install() {
   case "$uname_str" in
   Linux)
     if [ -f "/etc/debian_version" ]; then
+      echo "+ sudo apt update"
+      sudo apt update
       cat "$dir/debian_packages.txt" | while read line
       do
         echo "+ sudo apt install -y $line"
